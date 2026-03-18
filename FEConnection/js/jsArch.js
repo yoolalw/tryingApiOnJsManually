ef6d1fb25e5d3978d9598d66a3ac5ff3640f6525
yoolalw <larakemilyprofissional@gmail.com>
2026-03-18 16:44:06 -0300

    corrigindo erro - envio (304)

--- FEConnection/js/jsArch.js
+++ FEConnection/js/jsArch.js
@@ -1,19 +1,27 @@
-const formId = document.getElementById("formId");
 const submitBtn = document.getElementById("submitBtn");
+
+const formData = new FormData(document.getElementById("formId"));
 
 async function fetchTry() {
     try {
-        const response = await fetch('http://localhost:8080/produtos');
+        const response = await fetch('http://localhost:8080/produtos/register', {
+            "method": "POST",
+            headers : {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        });
         const data = await response.json();
-        console.log(`aaa - ${data}`);
-    } catch (err) {
-        console.error("aaah", err)
+        console.log(`pls js gfy - ${data}`);
+    } catch (error) {
+        console.error("Error:", error)
     }
 }
 
-submitBtn.addEventListener('submit', async function (e) {
+
+submitBtn.addEventListener('submit', async (e) => {
     e.preventDefault();
-    await fetchTry();
+    alert("asdsa")
+    fetchTry();
 });
 
-fetchTry();
