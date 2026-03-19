9e2f8aa32517c4bcf4c76579f12dbe014618b753
yoolalw <larakemilyprofissional@gmail.com>
2026-03-19 15:24:27 -0300

    CONECTEIIIII

--- FEConnection/js/jsArch.js
+++ FEConnection/js/jsArch.js
@@ -1,27 +1,44 @@
-const submitBtn = document.getElementById("submitBtn");
-
-const formData = new FormData(document.getElementById("formId"));
-
-async function fetchTry() {
-    try {
-        const response = await fetch('http://localhost:8080/produtos/register', {
-            "method": "POST",
-            headers : {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(formData)
-        });
-        const data = await response.json();
-        console.log(`pls js gfy - ${data}`);
-    } catch (error) {
-        console.error("Error:", error)
-    }
-}
+const formData = document.querySelector("#formId")
 
 
-submitBtn.addEventListener('submit', async (e) => {
+formData.addEventListener('submit', async function (e) {    
     e.preventDefault();
-    alert("asdsa")
-    fetchTry();
+    async function tryingThisFckJs() {
+
+        const codigo = document.getElementById('codigo').value
+        const nome = document.getElementById('nome').value
+        const preco = document.getElementById('preco').value
+        const quantidade = document.getElementById('quantidade').value
+
+
+        const produto = {
+            codigo: codigo,
+            nome: nome,
+            preco: preco,
+            quantidade: quantidade
+        }
+
+        try {
+            const response = await fetch('http://localhost:8080/produtos/register', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(produto)
+            });
+
+            const json = response.json
+
+            console.log(json);
+        } catch (error) {
+            console.error(" !! Error:", error)
+        }
+
+
+    }
+    tryingThisFckJs();
+
 });
+
+
 
